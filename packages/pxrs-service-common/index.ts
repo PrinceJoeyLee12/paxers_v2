eaeacd38127be8755a67fb2f90df5a77c4e53b59
Prince Joey Lee <prince.joey.lee@accenture.com>
2021-07-24 22:12:44 +0800

    [skip ci] chore: release
     - paxers@0.0.11
     - pxrs-service-common@0.0.5

--- packages/pxrs-service-common/index.ts
+++ packages/pxrs-service-common/index.ts
@@ -1,2 +1,2 @@
-export { SEND_ACCOUNT_ACTIVATION_EMAIL } from './EmailTemplates/emailTypes';
+export * from './EmailTemplates/emailTypes';
 export { default as renderFileType } from './EmailTemplates/renderEjsHtmllString';
